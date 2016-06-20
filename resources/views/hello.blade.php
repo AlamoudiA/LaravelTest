90968c71096daedaef645cc8731cab524a9f1233
AlamoudiA <aboudalamoudi@hotmail.com>
2016-06-20 16:49:01 -0400

    added route Controller

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello world for the 9000001th time</div>
+                <div class="title"> {!! $name !!} </div>
             </div>
         </div>
     </body>
